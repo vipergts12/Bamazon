dc04176ca33314b67aa4f82fed784603102667f5
Oscar Enriquez <macair@Oscars-Air.home>
2019-10-29 22:25:31 -0400

    Got code to work for customer but still getting error message after purchase.

--- customer.js
+++ customer.js
@@ -1,37 +1,77 @@
 var mysql = require("mysql");
-var inquirer = "inquirer";
-var table = require("cli-table2");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "aa224718",
-    database: "bamazon_db",
-    port: 3306
-})
+	host:"localhost",
+	port:3306,
+	user:"root",
+	password:"aa224718",
+	database:"bamazon_db"
+});
 
-connection.connect();
-var display = function () {
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        console.log("------------------");
-        console.log(" Welcome to My bamazon store");
-        console.log("----------------");
-        console.log("");
-        console.log("Find your product review:");
-        console.log("");
-    });
-    var Table = require('cli-table2');
+connection.connect(function(err){
+	if(err)throw err;
+	console.log("connected as id" + connection.threadId);
+});
 
-    // instantiate
-    var table = new Table({
-        head: ['TH 1 label', 'TH 2 label']
-        , colWidths: [100, 200]
-    });
-    for (var i = 0;  i < res.length; i ++){
-        table.push ([res[i].id, res[i].products_name, res[i].price]);
-    }
-        console.log(tabel.toString());
-        console.log("");
-    };
-    display();
+var displayProducts = function(){
+	var query = "Select * FROM products";
+	connection.query(query, function(err, res){
+		if(err) throw err;
+		var displayTable = new Table ({
+			head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
+			colWidths: [10,25,25,10,14]
+		});
+		for(var i = 0; i < res.length; i++){
+			displayTable.push(
+				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+				);
+		}
+		console.log(displayTable.toString());
+		purchasePrompt();
+	});
+}
+
+function purchasePrompt(){
+	inquirer.prompt([
+	{
+		name: "ID",
+		type: "input",
+		message:"Please enter Item ID you like to purhcase.",
+		filter:Number
+	},
+	{
+		name:"Quantity",
+		type:"input",
+		message:"How many items do you wish to purchase?",
+		filter:Number
+	},
+
+ ]).then(function(answers){
+ 	var quantityNeeded = answers.Quantity;
+ 	var IDrequested = answers.ID;
+ 	purchaseOrder(IDrequested, quantityNeeded);
+ });
+};
+
+function purchaseOrder(ID, amtNeeded){
+	connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
+		if(err){console.log(err)};
+		if(amtNeeded <= res[0].stock_quantity){
+			var totalCost = res[0].price * amtNeeded;
+			console.log("Good news your order is in stock!");
+			console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you!");
+
+			connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+		} else{
+			console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+		};
+		displayProducts();
+	});
+};
+
+displayProducts(); 
+
+
+
